c73edd7304f603acaa0912227c420585923ad99d
Hadrien de Cuzey <hadrien.de.cuzey@futurice.com>
2017-01-03 09:44:51 +0200

    Add test to ping responder

--- discord-bot/responders/ping/index.spec.ts
+++ discord-bot/responders/ping/index.spec.ts
@@ -1,9 +1,17 @@
-import { equal as assertEqual } from 'assert';
+import { equal as assertEqual, notEqual as assertNotEqual } from 'assert';
 import PingResponder from 'discord-bot/responders/ping';
 
 describe('discord-bot/responders/ping/index.spec.ts', () => {
 
   const responder = PingResponder();
+
+  it('only responds to PING', () => {
+    return Promise.resolve()
+      .then(() => responder('foo'))
+      .then(response => assertEqual(response, null))
+      .then(() => responder('ping'))
+      .then(response => assertNotEqual(response, null));
+  });
 
   it('responds PONG to PING', () => {
     return Promise.resolve()
